189e720f337ec5913715791228f80cb8c91fd908
vineesh23 <vinishb24@gmail.com>
2026-03-02 09:46:42 +0530

    feat: configure neon postgres database connection

---
+++ config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
+module.exports = sequelize;
